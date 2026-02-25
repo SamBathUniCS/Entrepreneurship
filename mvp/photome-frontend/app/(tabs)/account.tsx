ff021adf65888c98f5f077c7d79cc5c4d0dd7b57
Kithmini Joseph <kithminijoseph@gmail.com>
2026-02-25 01:51:49 +0000

    updated frontend and event search page

---
+++ mvp/photome-frontend/app/(tabs)/account.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Account() {
+  return (
+    <View style={{ flex: 1, padding: 16 }}>
+      <Text style={{ fontSize: 18, fontWeight: "800" }}>Account</Text>
+      <Text style={{ opacity: 0.7, marginTop: 8 }}>
+      </Text>
+    </View>
+  );
+}
